b3ccecc54201659eaf6366954d91296b1b8db87e
Hulkstern <rjbare3@gmail.com>
2021-02-24 17:57:02 -0500

    Edited some wording
    Missed some wording with the last edit

--- Testing.sh
+++ Testing.sh
@@ -15,7 +15,7 @@ function F2
 #getval=$(F2)  
 #echo $getval
 
-#printf "How the actual F*** did you manage to start a server with a negative\nindex??? Either you royally fucked up or you managed to find one\nhell of an edge case I hadn't accounted for.\n"
+#printf "How the actual F*** did you manage to start a server with a negative\nindex??? Either you royally f****d up or you managed to find one\nhell of an edge case I hadn't accounted for.\n"
 
 FullName=("firstName" "middleName" "lastName")
 echo "Hello ${FullName[2]}!"
